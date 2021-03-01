ad8435c5f03c0a2cd1939c00006642d853c4c5c7
Zach Smith <zsmith4331@gmail.com>
2021-02-28 19:10:02 -0500

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
-const { prompt } = require("inquirer")
+const { prompt } = require("inquirer");
+const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -14,3 +15,43 @@ const connection = mysql.createConnection({
     if (error) throw error;
     startApplicaiton();
   });
+
+  const startApplicaiton = () => {
+      inquirer.prompt({
+            type: "list",
+            message: "Where would you like to begin",
+            choice: [
+                "View all employees",
+                "Add new employee",
+                "Update existing employee",
+                "Exit applicaiton"
+            ]
+    
+      }).then((response) => {
+          if(response.choice === "View all employees") {
+              viewAllEmployees();
+          } else if (response.choice === "Add new employee") {
+              addNewEmployee();
+          } else if (response.choice === "Update existing employee") {
+              updateExistingEmployee();
+          } else {
+              connection.end();
+          }
+      });
+  };
+
+  const viewAllEmployees = () => {
+      inquirer.prompt([
+          {
+
+          }
+      ])
+  };
+
+  const addNewEmployee = () => { 
+
+  };
+
+  const updateExistingEmployee = () => {
+
+  };
